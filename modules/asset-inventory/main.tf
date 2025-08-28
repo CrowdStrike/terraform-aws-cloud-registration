f1ff66d58ddc4922a9090175eb95c8e3cf691d5f
prodsec-access-services <prodsec-access-services@users.noreply.github.com>
2025-08-28 18:58:30 +0000

    Update permissions in main.tf for asset-inventory module

--- modules/asset-inventory/main.tf
+++ modules/asset-inventory/main.tf
@@ -195,3 +195,4 @@ resource "aws_iam_role_policy_attachment" "this" {
   role       = aws_iam_role.this[count.index].name
   policy_arn = "arn:${data.aws_partition.current.partition}:iam::aws:policy/SecurityAudit"
 }
+#test
