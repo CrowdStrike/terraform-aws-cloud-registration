3ef3b87e63d0c238ea46b87709ea7263a70028fe
Matan Antebi <matan.antebi@crowdstrike.com>
2025-08-28 14:35:31 +0300

    change terraform deprecated region to dspm only

--- modules/sensor-management/main.tf
+++ modules/sensor-management/main.tf
@@ -4,7 +4,7 @@ data "aws_partition" "current" {}
 
 locals {
   account_id    = data.aws_caller_identity.current.account_id
-  aws_region    = data.aws_region.current.region
+  aws_region    = data.aws_region.current.name
   aws_partition = data.aws_partition.current.partition
 }
 
