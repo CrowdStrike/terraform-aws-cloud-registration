9c3b93d795d56c1d1e4a0c08f7d11e7dd4aa33e2
Jessica Kane <jessica.kane@crowdstrike.com>
2025-10-15 13:52:18 -0400

    teraform fmt

--- modules/dspm-roles/main.tf
+++ modules/dspm-roles/main.tf
@@ -19,7 +19,6 @@ resource "aws_ssm_parameter" "agentless_scanning_root_parameter" {
   value = jsonencode({
     version            = "1.0.0+tf.1"
     deployment_regions = var.dspm_regions
-    host_account_id    = data.aws_caller_identity.current.account_id
     scanner_role_arn   = aws_iam_role.crowdstrike_aws_dspm_scanner_role.arn
     instance_profile   = local.is_host_account ? aws_iam_instance_profile.instance_profile[0].name : ""
     host_account_id    = local.is_host_account ? data.aws_caller_identity.current.account_id : var.agentless_scanning_host_account_id
