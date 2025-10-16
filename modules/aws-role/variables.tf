051f8fdda0ab2720bcacf4a5a83c04665c32607c
AdarShakedCrowdstrike <adar.shaked@crowdstrike.com>
2025-10-16 14:40:39 +0300

    CSPG-67584 add gov validation to agentless_scanning_regions variable

--- modules/aws-role/variables.tf
+++ modules/aws-role/variables.tf
@@ -166,9 +166,10 @@ variable "agentless_scanning_regions" {
   validation {
     condition = alltrue([
       for region in var.agentless_scanning_regions :
-      can(regex("^(?:us|eu|ap|sa|ca|af|me|il)-(?:north|south|east|west|central|northeast|southeast|southwest|northwest)-[1-4]$", region))
+      (can(regex("^(?:us|eu|ap|sa|ca|af|me|il)-(?:north|south|east|west|central|northeast|southeast|southwest|northwest)-[1-4]$", region)) ||
+      can(regex("^us-gov-(?:east|west)-1$", region)))
     ])
-    error_message = "Each element in the agentless_scanning_regions list must be a valid AWS region (e.g., 'us-east-1', 'eu-west-2')."
+    error_message = "Each element in the agentless_scanning_regions list must be a valid AWS region (e.g., 'us-east-1', 'eu-west-2', 'us-gov-east-1', 'us-gov-west-1')."
   }
 }
 
