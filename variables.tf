e496cfaafa6eff30a4f3935c26a93e55a6ae94ab
Matthew Phillips <matthew.phillips@crowdstrike.com>
2025-11-17 12:59:06 -0500

    feat(terraform): AWS Registration - add S3-based Log Ingestion Method to RTV&D

--- variables.tf
+++ variables.tf
@@ -313,20 +313,24 @@ variable "log_ingestion_method" {
 
 variable "log_ingestion_s3_bucket_name" {
   type        = string
+  default     = ""
   description = "S3 bucket name containing CloudTrail logs (required when log_ingestion_method=s3)"
 }
 
 variable "log_ingestion_sns_topic_arn" {
   type        = string
+  default     = ""
   description = "SNS topic ARN that publishes S3 object creation events (required when log_ingestion_method=s3)"
 }
 
 variable "log_ingestion_s3_bucket_prefix" {
   type        = string
+  default     = ""
   description = "Optional S3 bucket prefix/path for CloudTrail logs (when log_ingestion_method=s3)"
 }
 
 variable "log_ingestion_kms_key_arn" {
   type        = string
+  default     = ""
   description = "Optional KMS key ARN for decrypting S3 objects (when log_ingestion_method=s3)"
 }
