98f29b894efe66b480ae386c11b444124f5215cb
Matan Antebi <matan.antebi@crowdstrike.com>
2026-06-11 12:08:36 +0300

    Inline permissions_boundary expression to match other modules

--- modules/agentless-scanning-roles/locals.tf
+++ modules/agentless-scanning-roles/locals.tf
@@ -6,8 +6,6 @@ locals {
 
   aws_partition = data.aws_partition.current.partition
 
-  permissions_boundary_arn = var.permissions_boundary != "" ? "arn:${local.aws_partition}:iam::${data.aws_caller_identity.current.account_id}:policy/${var.permissions_boundary}" : null
-
   # Extract VPC ARNs from custom VPC resources map for IAM policy conditions
   custom_vpc_arns = [
     for region, resources in var.agentless_scanning_custom_vpc_resources_map :
--- modules/agentless-scanning-roles/main.tf
+++ modules/agentless-scanning-roles/main.tf
@@ -79,7 +79,7 @@ resource "aws_iam_role" "crowdstrike_aws_agentless_scanning_scanner_role" {
       }
     ]
   })
-  permissions_boundary = local.permissions_boundary_arn
+  permissions_boundary = var.permissions_boundary != "" ? "arn:${local.aws_partition}:iam::${data.aws_caller_identity.current.account_id}:policy/${var.permissions_boundary}" : null
   tags = merge(
     var.tags,
     {
